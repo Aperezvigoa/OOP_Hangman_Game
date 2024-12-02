450e7b84d1df90c979e34c80d82af19c111f44ba
alexp <alexp@DESKTOP-UT96N89>
2024-12-02 21:38:50 +0100

    Agregar archivos de proyecto.

---
+++ Hangman/Program.cs
@@ -0,0 +1,186 @@
+﻿/*
+ * Clase Gestor Juego
+ * Leer palabras de un txt - ok
+ * Selecciona la palabra - ok
+ * Clase Palabra - Controla la palabra - ok
+ * Validacion de entrada - ok
+ * Registro Intentos
+ * Renderizado de ahorcado
+ * Interfaz de usuario
+ */
+
+using System.IO;
+
+var game = new HangMan();
+game.Details();
+game.RunGame();
+
+Console.ReadKey();
+
+
+class HangMan
+{
+    private ExtractingWords extractor;
+    private SelectingAWord _selectingWord;
+    private WordManager wordManager;
+    private InputManager inputManager;
+    private AttempManager attempManager;
+    public readonly string SelectedWord;
+    private List<string> words;
+
+    public HangMan()
+    {
+        extractor = new ExtractingWords();
+        words = new List<string>();
+        inputManager = new InputManager();
+        attempManager = new AttempManager();
+        // Reading words and add to list
+        words = extractor.ReadWordsFromFile(words);
+        // Selecting the random word
+        _selectingWord = new SelectingAWord(words);
+        // Saving the word
+        SelectedWord = _selectingWord.RandomWord;
+        // Upload to wordManager
+        wordManager = new WordManager(SelectedWord);
+    }
+
+    public void Details()
+    {
+        Console.WriteLine($"The word is {SelectedWord}");
+    }
+    public void RunGame()
+    {
+        int attemps = attempManager.RemainingAttempts();
+        while (attemps > 0)
+        {
+            Console.WriteLine($"Remaining attemps: {attemps}");
+            Console.WriteLine($"Word: {wordManager.UserWord}");
+            Console.WriteLine("Write a letter:");
+            string playerInput = Console.ReadLine();
+            char charPlayerInput;
+            if (InputManager.IsValidChar(playerInput))
+            { 
+                charPlayerInput = playerInput[0];
+                attemps = attempManager.UpdateRemainingAttempts(inputManager.IsCharUsed(charPlayerInput));
+                wordManager.RevealingWord(charPlayerInput);
+            }
+            continue;
+        }
+    }
+}
+
+public class ExtractingWords
+{
+    private StreamReader _sReader;
+    private string _lineToRead;
+    private readonly string _filePath;
+
+    public ExtractingWords(string path = "C:\\Users\\alexp\\Desktop\\HangmanWords.txt")
+    {
+        _filePath = path;
+        _sReader = new StreamReader(_filePath);
+    }
+
+    public List<string> ReadWordsFromFile(List<string> words)
+    {
+        _lineToRead = _sReader.ReadLine();
+        while (_lineToRead != null)
+        {
+            words.Add(_lineToRead.Trim());
+            _lineToRead = _sReader.ReadLine();
+        }
+        _sReader.Close();
+        return words;
+    }
+}
+
+
+public class SelectingAWord
+{
+    public readonly string RandomWord;
+
+    public SelectingAWord(List<string> words)
+    {
+        var random = new Random();
+        RandomWord = words[random.Next(0, words.Count)];
+    }
+}
+
+public class WordManager
+{
+    private readonly string _secretWord;
+    public string UserWord = "";
+
+    public WordManager(string SelectedWord)
+    {
+        _secretWord = SelectedWord;
+        foreach (char c in _secretWord) UserWord += '_';
+    }
+
+    public string RevealingWord(char userInputChar) 
+    {
+        List<int> positions = new List<int>();
+
+        for (int i = 0; i < _secretWord.Length; i++)
+        {
+            if (_secretWord[i] == userInputChar) positions.Add(i);
+        }
+
+        char[] tempWordArr = UserWord.ToCharArray();
+
+        if (positions.Count > 0) 
+        {
+            for (int i = 0; i < positions.Count; i++)
+            {
+                tempWordArr[positions[i]] = userInputChar;
+            }
+            UserWord = new string(tempWordArr);
+        }
+        return UserWord;
+    }
+}
+
+public class InputManager
+{
+
+    private List<char> WrongLetters; 
+
+    public InputManager()
+    {
+        WrongLetters = new List<char>();
+    }
+
+    public static bool IsValidChar(string input)
+    {
+        bool InputIsValid = Char.TryParse(input, out char inputChar);
+        if (InputIsValid)
+        {
+            if (string.IsNullOrEmpty(input)) return false;
+            else if (Char.IsLetter(inputChar)) return true;
+            return false;
+        }
+        return false;
+    }
+
+    public bool IsCharUsed (char c)
+    {
+        if(WrongLetters.Contains(c)) return true;
+        WrongLetters.Add(c);
+        return false;
+    }
+}
+
+public class AttempManager
+{
+    private int _maxAttemps;
+
+    public AttempManager() => _maxAttemps = 5;
+
+    public int RemainingAttempts() => _maxAttemps;
+
+    public int UpdateRemainingAttempts(bool isDuplicateLetter)
+    {
+        if (!isDuplicateLetter) --_maxAttemps;         
+        return _maxAttemps;
+    }
+}
